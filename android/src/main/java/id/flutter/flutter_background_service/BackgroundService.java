c3fc8ddfe69e0d4bbe4305fd25effda6a534b9df
DK Danish <mohammaddanish.khawaja@gmail.com>
2024-06-28 11:50:23 +0500

    new

--- android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
+++ android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
@@ -150,22 +150,21 @@ public class BackgroundService extends Service implements MethodChannel.MethodCa
     protected void updateNotificationInfo() {
         try {
             if (config.isForeground()) {
-                String packageName = getApplicationContext().getPackageName();
-                Intent i = getPackageManager().getLaunchIntentForPackage(packageName);
+                //String packageName = getApplicationContext().getPackageName();
+                //Intent i = getPackageManager().getLaunchIntentForPackage(packageName);
 
                 int flags = PendingIntent.FLAG_CANCEL_CURRENT;
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                     flags |= PendingIntent.FLAG_MUTABLE;
                 }
 
-                PendingIntent pi = PendingIntent.getActivity(BackgroundService.this, 11, i, flags);
+                //PendingIntent pi = PendingIntent.getActivity(BackgroundService.this, 11, i, flags);
                 NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, notificationChannelId)
                         .setSmallIcon(R.drawable.ic_bg_service_small)
                         .setAutoCancel(true)
                         .setOngoing(true)
                         .setContentTitle(notificationTitle)
-                        .setContentText(notificationContent)
-                        .setContentIntent(pi);
+                        .setContentText(notificationContent);
 
                 startForeground(notificationId, mBuilder.build());
             }
